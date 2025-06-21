63dd24b8ea73d7bc1355b571f2287d388fa7456f
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2025-06-21 14:59:00 +0100

    I Added a hero section and changed the global font to something more appealing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from './components/Hero'
+import './styles/App.css'
 
 function App() {
   return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ import styles from './Hero.module.css'
 function Hero() {
     return (
         <section className={styles.hero}>
-            <h1 className={styles.title}> Hi, Im Lawri Darbyshire</h1>
-            <h2 className={styles.subtitle}>Develeper | Stage Lighting Enthusaist | Volentier</h2>
-            <p className={styles.tagline}> I like to build projects that solve problems, I like to do lighting for my school, I like to Volentier at Greenham Common </p>
+            <h1 className={styles.title}>Lawri Darbyshire</h1>
+            <h2 className={styles.subtitle}>Developer | Stage Lighting Enthusaist</h2>
+            <p className={styles.tagline}> I like to build projects that solve problems, I like to do lighting for my school, I like to Volunteer at Greenham Common </p>
         </section>
     )
 }
