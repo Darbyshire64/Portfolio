e9fb4a92149c6b2409ef7429815e225d726ef5e6
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2025-06-21 15:13:10 +0100

    I got a basic tag line cycling effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import styles from './Hero.module.css'
-
+import { useState, useEffect } from 'react'
 
 
 function Hero() {
+    const tagLines = [
+        'I like to build projects that solve problems.',
+        'I like to do Lighting for my school.',
+        'I like to volunteer for Greenham Common Control Tower'
+    ]
+
+    const [current, setCurrent] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent((prev) => (prev + 1) % tagLines.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
     return (
         <section className={styles.hero}>
             <h1 className={styles.title}>Lawri Darbyshire</h1>
             <h2 className={styles.subtitle}>Developer | Stage Lighting Enthusaist</h2>
-            <p className={styles.tagline}> I like to build projects that solve problems, I like to do lighting for my school, I like to Volunteer at Greenham Common </p>
+            <p className={styles.tagline}>{tagLines[current]}</p>
         </section>
     )
 }
