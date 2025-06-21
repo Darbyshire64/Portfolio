752d89fe53b98b453d14a5ffe8ff26dcb3285ace
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2025-06-21 15:38:47 +0100

    After some long work i finaly got a nice type writer animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,28 +4,57 @@ import { useState, useEffect } from 'react'
 
 
 function Hero() {
-    const tagLines = [
+    const taglines = [
         'I like to build projects that solve problems.',
         'I like to do Lighting for my school.',
         'I like to volunteer for Greenham Common Control Tower'
     ]
 
-    const [current, setCurrent] = useState(0);
+    const [displayed, setDisplayed] = useState('')
+    const [taglineIndex, setTaglineIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrent((prev) => (prev + 1) % tagLines.length);
-        }, 3000);
+        const current = taglines[taglineIndex];
+        let timeout;
 
-        return () => clearInterval(interval);
-    }, []);
+        if (isDeleting) {
+            timeout = setTimeout(() => {
+                setDisplayed(current.substring(0, charIndex - 1))
+                setCharIndex(charIndex-1);
+            }, 50);
+        } else {
+            timeout = setTimeout(() => {
+                setDisplayed(current.substring(0, charIndex + 1));
+                setCharIndex(charIndex + 1);
+            }, 100);
+        }
+
+        if (!isDeleting && charIndex === current.length) {
+            setTimeout(() => setIsDeleting(true), 2000);
+            return;
+        }
+
+        if (isDeleting && charIndex === 0) {
+            setIsDeleting(false);
+            setTaglineIndex((prev) => (prev + 1) % taglines.length);
+        }
+
+        return () => clearInterval(timeout);
+    }, [charIndex, isDeleting, taglines, taglineIndex]);
 
 
     return (
         <section className={styles.hero}>
             <h1 className={styles.title}>Lawri Darbyshire</h1>
             <h2 className={styles.subtitle}>Developer | Stage Lighting Enthusaist</h2>
-            <p className={styles.tagline}>{tagLines[current]}</p>
+            <p className={styles.tagline}>
+                {displayed}
+                <span className={styles.cursor}>▌</span> 
+            </p>
         </section>
     )
 }
