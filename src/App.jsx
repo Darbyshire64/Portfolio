c10635e63ac325e38faaffae672d6eb64bddc331
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2025-06-22 14:37:40 +0100

    Created a nav bar and removed public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
+import Navbar from './components/Navbar'
 import './styles/App.css'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Hero />
       <About />
       <Projects />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import styles from './Navbar.module.css';
+import React, { useState, useEffect, useRef} from 'react';
+
+function Navbar() {
+    const [hidden, setHidden] = useState(false);
+    const lastScrollY = useRef(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY.current && currentScrollY > 100) {
+                setHidden(true);
+            } else {
+                setHidden(false);
+            }
+
+            lastScrollY.current = currentScrollY;
+        };
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <section className={`${styles.navbar} ${hidden ? styles.hidden : ''}`}>
+            <div className={styles.navLinks}>
+                <a href="#About">About</a>
+                <a href="#Projects">Projects</a>
+            </div>
+        </section>
+    )
+}
+
+export default Navbar;
