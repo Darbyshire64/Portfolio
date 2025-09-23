16843ac391c10a30da668fd05d4d8cc5346c21a9
Darbyshire64 <lawri@lawridarbyshire.co.uk>
2025-09-23 17:30:00 +0100

    Updated  Footer Text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <section className={styles.Footer}>
-            <p className={styles.FooterText}>© Lawri Darbyshire 2025 | All Rights Reserverd</p>
+            <p className={styles.FooterText}>© Lawri Darbyshire 2023-2025 | All Rights Reserverd</p>
+            <p className={Styles.FooterText}>This Site is protected via Cloudflare®</p>
         </section>        
     )
 }
