6b20072f782b1d2e4663d221fbfa27a4d375b807
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2026-05-07 00:52:53 +0100

    Update footer copyright year and add image notice

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <section className={styles.Footer}>
-            <p className={styles.FooterText}>© Lawri Darbyshire 2023-2025 | All Rights Reserverd</p>
+            <p className={styles.FooterText}>© Lawri Darbyshire 2026 | All Rights Reserverd</p>
+            <p className={styles.FooterText}> Images Property of there Respective Owners.</p>
             <p className={styles.FooterText}>This Site is protected via Cloudflare®</p>
         </section>        
     )
