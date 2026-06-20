0a6b7a2c2b1fc96212cb11439d8a0e356a4d9d49
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2026-06-21 00:16:58 +0100

    Updated Footer Text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,6 @@ function Footer() {
     return (
         <section className={styles.Footer}>
             <p className={styles.FooterText}>© Lawri Darbyshire 2026 | All Rights Reserverd | Images Are Property of Respective Owners</p>
-            <p className={styles.FooterText}>Analyitics are Collected in the intrest of site diagnostics.</p>
-            <p className={styles.FooterText}>This Site is protected via Cloudflare® & CrowdSec</p>
         </section>        
     )
 }
