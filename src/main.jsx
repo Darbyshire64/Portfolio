e57ba7ff3caa320852d26d76640e0aef5d948994
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2026-06-20 22:29:38 +0100

    Add UmamiTracker component for pageview tracking

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './styles/variables.css' 
 
+function UmamiTracker() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if (window.umami) {
+      window.umami.track('pageview', {
+        url: location.pathname + location.search
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <UmamiTracker />
     <App />
   </BrowserRouter>,
 );
