da5ca86bd63fef28d93d19f5029912e6f4f01d0f
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2026-06-20 23:29:49 +0100

    Update footer content and fix text errors
    Consolidate footer text and correct typos.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <section className={styles.Footer}>
-            <p className={styles.FooterText}>© Lawri Darbyshire 2026 | All Rights Reserverd</p>
-            <p className={styles.FooterText}> Images Property of there Respective Owners.</p>
-            <p className={styles.FooterText}>This Site is protected via Cloudflare®</p>
+            <p className={styles.FooterText}>© Lawri Darbyshire 2026 | All Rights Reserverd | Images Are Property of Respective Owners</p>
+            <p className={styles.FooterText}>Analyitics are Collected in the intrest of site diagnostics.</p>
+            <p className={styles.FooterText}>This Site is protected via Cloudflare® & CrowdSec</p>
         </section>        
     )
 }
