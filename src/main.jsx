16a215cea0c0271965c27d3bd2e3aadd54c36aa5
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2026-06-20 22:36:43 +0100

    Refactor main.jsx to include UmamiTracker component

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import './styles/variables.css' 
+import App from './App.jsx';
+import './styles/variables.css';
 
-function UmamiTracker() {
-  const location = useLocation();
-
-  React.useEffect(() => {
-    if (window.umami) {
-      window.umami.track('pageview', {
-        url: location.pathname + location.search
-      });
-    }
-  }, [location]);
-
-  return null;
-}
+import UmamiTracker from './UmamiTracker.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <UmamiTracker />
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
